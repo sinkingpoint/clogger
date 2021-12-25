1f2598f8a65689676ae26c206edd423895b3ae34
sinkingpoint <colin@quirl.co.nz>
2021-12-25 15:05:48 +1100

    Add functionality to handle failures
    This commit adds in some functionality to automatically handle failures. In particular,
    we have a state machine process:
    
    1. We send messages to the output
    2. If it fails with a TRANSIENT_FAILURE, we retry immediatly with an exponential backoff
    3. If the exponential backoff fails, or we fail with LONG_FAILURE, we go to LONG_FAILURE mode
    4. If we are in LONG_FAILURE mode, we send to an alternate outputter, or drop the data
    5. If we are in LONG_FAILURE mode, and haven't tried sending data in a while (30 seconds), we move back into SUCCESS and retry from the beginning
    6. Upon any success, return to normal mode

--- internal/outputs/sender.go
+++ internal/outputs/sender.go
@@ -2,16 +2,26 @@ package outputs
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/rs/zerolog/log"
 	"github.com/sinkingpoint/clogger/internal/clogger"
 	"github.com/sinkingpoint/clogger/internal/tracing"
 	"go.opentelemetry.io/otel/attribute"
 )
 
+type RetryConfig struct {
+	MaxBackOffTries int
+	BufferChannel   chan []clogger.Message
+	currentState    OutputResult
+	lastRetryTime   time.Time
+}
+
 // Sender encapsulates the functionality that all Outputters get for free i.e. Buffering
 type Sender struct {
 	SendConfig
+	RetryConfig
 	sender        Outputter
 	buffer        []clogger.Message
 	lastFlushTime time.Time
@@ -19,7 +29,11 @@ type Sender struct {
 
 func NewSender(config SendConfig, logic Outputter) *Sender {
 	return &Sender{
-		SendConfig:    config,
+		SendConfig: config,
+		RetryConfig: RetryConfig{
+			MaxBackOffTries: 5, // arbitrary, just for testing. Must make this configurable
+			currentState:    OUTPUT_SUCCESS,
+		},
 		buffer:        make([]clogger.Message, 0, config.BatchSize),
 		lastFlushTime: time.Now(),
 		sender:        logic,
@@ -39,10 +53,54 @@ func (s *Sender) queueMessages(ctx context.Context, messages []clogger.Message) 
 		s.buffer = append(s.buffer, messages[:remainingRoom]...)
 		s.Flush(ctx, false)
 		messages = messages[remainingRoom:]
-		s.buffer = s.buffer[:0]
 	}
 
 	s.buffer = append(s.buffer, messages...)
+}
+
+// handleLongFailure handles the buffer in the event that the main sender fails
+func (s *Sender) handleLongFailure(ctx context.Context) error {
+	s.currentState = OUTPUT_LONG_FAILURE
+
+	if s.BufferChannel != nil {
+		s.BufferChannel <- s.buffer
+	}
+
+	s.buffer = s.buffer[:0]
+
+	return nil
+}
+
+// doExponentialRetry handles the case where we have transient failures that can be retried
+// Note: This has the potential to cause double counting of logs (at least once delivery)
+func (s *Sender) doExponentialRetry(ctx context.Context) error {
+	// start at one because we assume we've already done one attempt at flushing
+	// to get here
+	backoffTime := time.Millisecond * 100
+
+	for i := 1; i < s.MaxBackOffTries; i++ {
+		time.Sleep(backoffTime)
+
+		result, err := s.sender.FlushToOutput(ctx, s.buffer)
+		if err != nil {
+			log.Debug().Err(err).Int("output_result", int(result)).Msg("Failed to flush output")
+		}
+
+		switch result {
+		case OUTPUT_SUCCESS:
+			s.buffer = s.buffer[:0]
+			s.lastFlushTime = time.Now()
+			s.currentState = OUTPUT_SUCCESS
+			return nil
+		case OUTPUT_TRANSIENT_FAILURE:
+			backoffTime *= 2
+			continue
+		case OUTPUT_LONG_FAILURE:
+			return s.handleLongFailure(ctx)
+		}
+	}
+
+	return fmt.Errorf("did a backoff without success")
 }
 
 // Flush flushes the current buffer to the output stream
@@ -55,15 +113,43 @@ func (s *Sender) Flush(ctx context.Context, final bool) {
 		attribute.String("last_flush_time", s.lastFlushTime.Format(time.RFC3339)),
 		attribute.Int("messages_in_queue", len(s.buffer)),
 	)
-	// If we aren't at the buffer limit, and we have flushed recently, and this isn't the final flush, just shortcircuit
-	if time.Since(s.lastFlushTime) < s.FlushInterval && len(s.buffer) < s.BatchSize && !final {
+
+	enoughTimeSinceLastFlush := time.Since(s.lastFlushTime) >= s.FlushInterval
+	reachedBufferLimit := len(s.buffer) >= s.BatchSize
+	if !enoughTimeSinceLastFlush && !reachedBufferLimit && !final {
 		span.AddEvent("Skipping Flush - not ready yet")
 		return
 	}
 
 	if len(s.buffer) > 0 {
-		s.sender.FlushToOutput(ctx, s.buffer)
-		s.buffer = s.buffer[:0]
-		s.lastFlushTime = time.Now()
+		// Don't do any exponential backoff or anything if we know that we're in a long failure
+		// just buffer it, but retry every minute or so incase we're back
+		if s.currentState == OUTPUT_LONG_FAILURE && time.Since(s.lastRetryTime) < 30*time.Second {
+			s.handleLongFailure(ctx)
+			return
+		}
+
+		s.lastRetryTime = time.Now()
+
+		result, err := s.sender.FlushToOutput(ctx, s.buffer)
+		if err != nil {
+			// We just log errors - retries etc should be controlled by the OutputResult return
+			log.Debug().Err(err).Int("output_result", int(result)).Msg("Failed to flush output")
+		}
+
+		switch result {
+		case OUTPUT_SUCCESS:
+			s.buffer = s.buffer[:0]
+			s.lastFlushTime = time.Now()
+			s.currentState = OUTPUT_SUCCESS
+		case OUTPUT_TRANSIENT_FAILURE:
+			err := s.doExponentialRetry(ctx)
+			if err != nil {
+				log.Warn().Err(err).Msg("Fell through trying to do exponential backoff")
+				s.handleLongFailure(ctx)
+			}
+		case OUTPUT_LONG_FAILURE:
+			s.handleLongFailure(ctx)
+		}
 	}
 }
